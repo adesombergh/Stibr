bfa74475281da22df82dac6117e8e6eeb68d8b6d
Aldebaran Desombergh <aldealdo@gmail.com>
2018-01-29 23:02:59 +0100

    Brought Token Automation

--- app/Http/Controllers/Transport.php
+++ app/Http/Controllers/Transport.php
@@ -14,18 +14,20 @@ class Transport extends Controller
     public function lines()
     {
         $lines = $this->getLines();
+        dump($this->getToken());
         return view('lines')->with('lines',$lines);
     }
 
     public function line($id)
     {
-        // $positions = $this->getPositionsByLine($id);
+        $positions = $this->getPositionsByLine($id);
+        dump($positions);
         // return view('line')->with(['line'=>$positions->lines[0]->vehiclePositions,'id'=>$id]);
     }
 
     public function getLines()
     {
-        $json = file_get_contents('routes.json');
+        $json = file_get_contents( storage_path('stib/routes.json') );
         $obj = json_decode($json);
         return $obj->routes;
     }
@@ -37,15 +39,15 @@ class Transport extends Controller
      */
     public function getPositionsByLine($line)
     {
+        $token = $this->getToken();
         $options = array(
             CURLOPT_URL             => "https://opendata-api.stib-mivb.be/OperationMonitoring/1.0/VehiclePositionByLine/$line",
             CURLOPT_HTTPHEADER      => array(
                                         'Accept: application/json',
-                                        'Authorization: Bearer ec3fbc12115e0fde38402cc5c72fd6af'
+                                        "Authorization: Bearer $token"
                                     ),
             CURLOPT_RETURNTRANSFER  => true // Returns output instead of printing it
         );
-
         //INIT CURL
         $curl = curl_init();
         curl_setopt_array($curl, $options);
@@ -61,5 +63,32 @@ class Transport extends Controller
         return $data;
     }
 
+
+
+    public function getToken()
+    {
+        $options = array(
+            CURLOPT_URL             => "https://opendata-api.stib-mivb.be/token",
+            CURLOPT_HTTPHEADER      => array(
+                                        'Authorization: Basic cVpTQnRCb0hOR2w2eEZZVzltS1VKZjNtY1h3YTpKSUdaTUl6Rm85ZlY4cTB3VTRIbEsyTmxmRGth'
+                                    ),
+            CURLOPT_POST            => 1,
+            CURLOPT_POSTFIELDS      => 'grant_type=client_credentials',
+            CURLOPT_RETURNTRANSFER  => true // Returns output instead of printing it
+        );
+        //INIT CURL
+        $curl = curl_init();
+        curl_setopt_array($curl, $options);
+        
+        // EXCUTE COMMANDE CURL. Stocke Résultats
+        $json = curl_exec($curl);
+
+        //CLOSE CURL (c'est mieux!) 
+        curl_close($curl);
+
+        // Decode les résultats pour travailler en php.
+        $data = json_decode($json);
+        return $data->access_token;
+    }
 
 }
